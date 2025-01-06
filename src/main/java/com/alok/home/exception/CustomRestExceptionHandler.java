e6739ce0ce04815c06bb7fe8e5fedd6f6f5d00f2
alokkusingh <alok.ku.singh@gmail.com>
2025-01-06 10:17:19 +0530

    Handling XIRR Overflow exception

--- src/main/java/com/alok/home/exception/CustomRestExceptionHandler.java
+++ src/main/java/com/alok/home/exception/CustomRestExceptionHandler.java
@@ -1,8 +1,26 @@
 package com.alok.home.exception;
 
 import com.alok.home.commons.dto.exception.GlobalRestExceptionHandler;
+import org.decampo.xirr.OverflowException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
+import java.time.ZonedDateTime;
 
 @RestControllerAdvice
 public class CustomRestExceptionHandler extends GlobalRestExceptionHandler {
+
+    @ExceptionHandler(OverflowException.class)
+    ProblemDetail handleException(OverflowException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        problemDetail.setTitle("Server Error");
+        problemDetail.setType(URI.create("home-api/errors/server-error"));
+        problemDetail.setProperty("errorCategory", "ServerError");
+        problemDetail.setProperty("timestamp", ZonedDateTime.now());
+        e.printStackTrace();
+        return problemDetail;
+    }
 }
